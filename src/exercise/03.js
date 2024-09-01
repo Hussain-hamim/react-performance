e04ae5cce261207d0321caef488a0b30e131b5e8
Hussain Hamim <mohammadhussainafghan83@gmail.com>
2024-09-01 22:04:20 +0430

    pass only primitive values

--- src/exercise/03.js
+++ src/exercise/03.js
@@ -1,6 +1,6 @@
 // React.memo for reducing unnecessary re-renders
-// 💯 Use a custom comparator function
-// http://localhost:3000/isolated/final/03.extra-1.js
+// 💯 pass only primitive values
+// http://localhost:3000/isolated/final/03.extra-2.js
 
 import * as React from 'react'
 import {useCombobox} from '../use-combobox'
@@ -22,8 +22,8 @@ function Menu({
           getItemProps={getItemProps}
           item={item}
           index={index}
-          selectedItem={selectedItem}
-          highlightedIndex={highlightedIndex}
+          isSelected={selectedItem?.id === item.id}
+          isHighlighted={highlightedIndex === index}
         >
           {item.name}
         </ListItem>
@@ -37,46 +37,25 @@ function ListItem({
   getItemProps,
   item,
   index,
-  selectedItem,
-  highlightedIndex,
+  isHighlighted,
+  isSelected,
   ...props
 }) {
-  const isSelected = selectedItem?.id === item.id
-  const isHighlighted = highlightedIndex === index
   return (
     <li
       {...getItemProps({
         index,
         item,
         style: {
-          fontWeight: isSelected ? 'bold' : 'normal',
           backgroundColor: isHighlighted ? 'lightgray' : 'inherit',
+          fontWeight: isSelected ? 'bold' : 'normal',
         },
         ...props,
       })}
     />
   )
 }
-ListItem = React.memo(ListItem, (prevProps, nextProps) => {
-  // true means do NOT rerender
-  // false means DO rerender
-
-  // these ones are easy if any of these changed, we should re-render
-  if (prevProps.getItemProps !== nextProps.getItemProps) return false
-  if (prevProps.item !== nextProps.item) return false
-  if (prevProps.index !== nextProps.index) return false
-  if (prevProps.selectedItem !== nextProps.selectedItem) return false
-
-  // this is trickier. We should only re-render if this list item:
-  // 1. was highlighted before and now it's not
-  // 2. was not highlighted before and now it is
-  if (prevProps.highlightedIndex !== nextProps.highlightedIndex) {
-    const wasPrevHighlighted = prevProps.highlightedIndex === prevProps.index
-    const isNowHighlighted = nextProps.highlightedIndex === nextProps.index
-    return wasPrevHighlighted === isNowHighlighted
-  }
-  return true
-})
+ListItem = React.memo(ListItem)
 
 function App() {
   const forceRerender = useForceRerender()
